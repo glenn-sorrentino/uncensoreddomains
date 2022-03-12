6b179a77b663e8b8d0a04980c661ca3b198d3c1e
Glenn <glenn.sorrentino@pm.me>
2022-03-12 13:59:22 -0800

    Initial commit

---
+++ v1/components/header/js/header.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $('.navButton').on('click', function() {
+    $('.staged').toggleClass('open');
+  });
+
+  $('.closeButton').on('click', function() {
+    $('.staged').removeClass('open');
+  });
+
+  $('NAV li').on('click', function() {
+    $('.staged').removeClass('open');
+  });
+
+});
